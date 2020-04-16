ec0c9e81679363dc2a5db1f5776586308b00d250
Karina Halevy <karina.halevy@gmail.com>
2020-04-16 09:43:18 -0700

    yeet

--- aTMcomponents.ml
+++ aTMcomponents.ml
@@ -33,7 +33,7 @@ let initialize (acc : account_spec list) : unit =
 (* acquire_id () -- Requests from the ATM customer and returns an id
    (akin to entering one's ATM card), by prompting for an id number
    and reading an id from stdin. *)
-val acquire_id () = read_int ;;
+let acquire_id () = read_int ;;
 
 (* acquire_amount () -- Requests from the ATM customer and returns an
    amount by prompting for an amount and reading an int from stdin. *)

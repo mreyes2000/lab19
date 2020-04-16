35e42a3292092fe6ea935b81d241132943d878be
Karina Halevy <karina.halevy@gmail.com>
2020-04-16 10:02:56 -0700

    types

--- aTMcomponents.ml
+++ aTMcomponents.ml
@@ -20,6 +20,17 @@ balance.
 (* initialize accts -- Establishes a database of accounts, each with a
    name, aribtrary id, and balance. The names and balances are
    initialized as per the `accts` provided. *)
+type id = int
+
+type action =
+  | Balance           (* balance inquiry *)
+  | Withdraw of int   (* withdraw an amount *)
+  | Deposit of int    (* deposit an amount *)
+  | Next              (* finish this customer and move on to the next one *)
+  | Finished          (* shut down the ATM and exit entirely *)
+;;
+
+type account_spec = {name : string; id : id; balance : int} ;;
 
 let database = ref [] ;;
 

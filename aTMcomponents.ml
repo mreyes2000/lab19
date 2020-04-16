90ab72f89ead577f263b3f85f49f652c6ac9cbc0
Karina Halevy <karina.halevy@gmail.com>
2020-04-16 09:46:48 -0700

    resolved

--- aTMcomponents.ml
+++ aTMcomponents.ml
@@ -33,13 +33,9 @@ let initialize (acc : account_spec list) : unit =
 (* acquire_id () -- Requests from the ATM customer and returns an id
    (akin to entering one's ATM card), by prompting for an id number
    and reading an id from stdin. *)
-<<<<<<< HEAD
-let acquire_id () = 
+let acquire_id () =
   Printf.printf("Please enter an ID: \n");
   read_int ();;
-=======
-let acquire_id () = read_int ;;
->>>>>>> ec0c9e81679363dc2a5db1f5776586308b00d250
 
 (* acquire_amount () -- Requests from the ATM customer and returns an
    amount by prompting for an amount and reading an int from stdin. *)
@@ -73,7 +69,7 @@ let get_name (i : id) : string =
 
 (* update_balance id amount -- Modifies the balance of the customer
    account with the given id, setting it to the given amount. *)
-let update_balance (n : id) (i : int) : unit = 
+let update_balance (n : id) (i : int) : unit =
   get_balance n;;
 
 (*....................................................................
